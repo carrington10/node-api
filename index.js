75d7371af95ac112ea3a5f2f0dc75bf0a278aee4
plV7350 <carrington10@ksu.edu>
2018-12-19 13:07:30 -0600

    adding in queries

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const bodyParser = require('body-parser')
-
+const db = require('./queries')
 const app = express()
 
 const port = 5000
@@ -19,6 +19,8 @@ app.use(
   })
 
 )
+
+app.get('/users',db.getUsers)
 
 
 app.listen(port, () => {
